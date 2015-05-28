e2224a0f5442e648fd952f7ffda8573852ee3bc8
Bartosz Balis <balis@agh.edu.pl>
2015-05-28 10:18:09 +0200

    Minor updates

--- webcrawler/src/main/scala/WebCrawler.scala
+++ webcrawler/src/main/scala/WebCrawler.scala
@@ -5,20 +5,17 @@ object WebCrawler extends App {
   import org.htmlcleaner.HtmlCleaner
   import java.net.URL
 
-  //val url = "http://www.icsr.agh.edu.pl/~balis"
   val url = "http://google.com"
+  val html = Source.fromURL(url)
 
   val cleaner = new HtmlCleaner
   val props = cleaner.getProperties
-
-  val html = Source.fromURL(url)
 
   val rootNode = cleaner.clean(html.mkString) 
   // mozna tez od razu z URLa: cleaner.clean(new URL(url))
 
   val elements = rootNode.getElementsByName("a", true) 
-
-  elements.toList map { elem => 
+  elements map { elem => 
     val url = elem.getAttributeByName("href")
     println(url.toString) 
   }
