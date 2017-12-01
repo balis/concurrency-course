33a475820552de9b82dc32e0668716742d73c759
Bartosz Balis <balis@agh.edu.pl>
2017-12-01 11:15:35 +0100

    Update Futures1.scala

--- tutorial/src/main/scala/twtutorial/Futures1.scala
+++ tutorial/src/main/scala/twtutorial/Futures1.scala
@@ -8,9 +8,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Futures1 extends App {
 
-  // used by 'time' method
-  implicit val baseTime = System.currentTimeMillis
-
   // 1. Create a Future - asynchronous computation
   val f = Future {
       Thread.sleep(500)
@@ -19,7 +16,7 @@ object Futures1 extends App {
 
   // 2. Await for its result. 
   // Blocking -- SHOULD NOT be used!
-  val result = Await.result(f, 1 second)
+  val result = Await.result(f, 1 second) // 1 second == 1.second() + implicit conversion
   println(result)
   Thread.sleep(1000)
 
